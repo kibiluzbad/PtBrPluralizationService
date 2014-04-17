a9009eb927ea8a71cb89bb6027ff92cc13099244
Junior Oliveira <junolive@gmail.com>
2014-04-17 02:16:25 -0300

    Acento tônico: oxítonas, paroxítonas e proparoxítonas.

---
+++ src/PluralizationServices.Tests/Extensions/TonicAccentExtensionTests.cs
@@ -0,0 +1,17 @@
+﻿using PluralizationServices.Extensions;
+using Xunit;
+
+namespace PluralizationServices.Tests.Extensions {
+
+    [Trait("Acento tônico", "Oxítonas, Paroxítonas e Proparoxítonas")]
+    public class TonicAccentExtensionTests
+    {
+        [Fact(DisplayName = "Oxítonas ou agudas")]
+        public void OxitonasOuAgudas() 
+        {
+            const string word = "você";
+
+            Assert.True(word.IsOxítona());
+        }
+    }
+}
---
+++ src/PluralizationServices/Extensions/TonicAccentExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PluralizationServices.Extensions 
+{
+    public static class TonicAccentExtension 
+    {
+        public static bool IsOxítona(this string word)
+        {
+            var lastPosition = word.Length - 1;
+            var lastCharacter = '\0';
+            if (lastPosition > 0) lastCharacter = char.ToLower(word[lastPosition]);
+
+            var penultimatePosition = word.Length - 2;
+            var penultimateCharacter = '\0';
+            if (penultimatePosition > 0) penultimateCharacter = char.ToLower(word[penultimatePosition]);
+
+            if (lastCharacter.Equals('s') && penultimateCharacter.HasAccent()) return true;
+            if (lastCharacter.HasAccent()) return true;
+
+            return false;
+        }
+
+        public static bool IsParoxítona(this string word)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static bool IsProparoxítonas(this string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
