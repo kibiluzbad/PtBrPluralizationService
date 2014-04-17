6dfaed73fe352890fbfcc9a787e6f9d8e7e65d2c
Junior Oliveira <junolive@gmail.com>
2014-04-17 02:18:24 -0300

    Fonema: vogais e consoantes

---
+++ src/PluralizationServices.Tests/Extensions/PhonemeExtensionTests.cs
@@ -0,0 +1,57 @@
+﻿using PluralizationServices.Extensions;
+using Xunit;
+
+namespace PluralizationServices.Tests.Extensions
+{
+    [Trait("Fonema", "Vogal e consoante")]
+    public class PhonemeExtensionTests
+    {
+        [Fact(DisplayName = "Vogal a")]
+        public void Vogala() 
+        {
+            const char letter = 'a';
+
+            Assert.True(letter.IsVowel());
+        }
+
+        [Fact(DisplayName = "Vogal A")]
+        public void VogalA() 
+        {
+            const char letter = 'A';
+
+            Assert.True(letter.IsVowel());
+        }
+
+        [Fact(DisplayName = "Vogal ã")]
+        public void Vogalã() 
+        {
+            const char letter = 'ã';
+
+            Assert.True(letter.IsVowel());
+        }
+
+        [Fact(DisplayName = "Vogal Ã")]
+        public void VogalÃ() 
+        {
+            const char letter = 'Ã';
+
+            Assert.True(letter.IsVowel());
+        }
+
+        [Fact(DisplayName = "Vogal b")]
+        public void Consoanteb() 
+        {
+            const char letter = 'b';
+
+            Assert.True(letter.IsConsonant());
+        }
+
+        [Fact(DisplayName = "Vogal B")]
+        public void VogalB() 
+        {
+            const char letter = 'B';
+
+            Assert.True(letter.IsConsonant());
+        }
+    }
+}
---
+++ src/PluralizationServices/Extensions/PhonemeExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PluralizationServices.Extensions 
+{
+    public static class PhonemeExtension
+    {
+        private const RegexOptions OptionsRegex = RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline;
+
+        public static bool HasAccent(this char letter)
+        {
+            var input = letter.ToString(CultureInfo.InvariantCulture);
+
+            const string vowelsWithAccentsPattern = "[áéíóúàèìòùâêîôûäëïöüãõ]";
+
+            return Regex.IsMatch(input, vowelsWithAccentsPattern, OptionsRegex);
+        }
+
+        public static bool IsVowel(this char letter)
+        {
+            var input = letter.ToString(CultureInfo.InvariantCulture);
+
+            if (HasAccent(letter)) return true;
+
+            const string vowelsWithoutAccentsPattern = "[aeiou]";
+            if (Regex.IsMatch(input, vowelsWithoutAccentsPattern, OptionsRegex)) return true;
+
+            return false;
+        }
+
+        public static bool IsConsonant(this char letter)
+        {
+            if (letter.Equals('\0')) return true;
+            if (!char.IsDigit(letter) && !IsVowel(letter)) return true;
+
+            return false;
+        }
+
+        public static bool In(this char letter, char[] collection)
+        {
+            return collection.Any(item => item.Equals(letter));
+        }
+    }
+}
