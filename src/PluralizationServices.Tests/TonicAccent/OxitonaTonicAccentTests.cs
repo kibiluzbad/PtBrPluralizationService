5ffee91149c4604a5b5fd95d81cb5b89498bb6c1
Junior Oliveira <junolive@gmail.com>
2014-07-15 22:52:41 -0300

    Acento tônico: oxítonas (refactor)

--- src/PluralizationServices.Tests/TonicAccent/OxitonaTonicAccentTests.cs
+++ src/PluralizationServices.Tests/TonicAccent/OxitonaTonicAccentTests.cs
@@ -14,7 +14,7 @@ namespace PluralizationServices.Tests.TonicAccent {
             _tonicAccent = new OxitonaTonicAccent();
         }
 
-        [Theory(DisplayName = "Terminadas em a, e ou o")]
+        [Theory(DisplayName = "Terminadas em á, â, é, ê, ó ou ô")]
         [InlineData("cajá")]
         [InlineData("você")]
         [InlineData("dominó")]
@@ -23,7 +23,7 @@ namespace PluralizationServices.Tests.TonicAccent {
             Assert.True(_tonicAccent.Verify(word));
         }
 
-        [Theory(DisplayName = "Terminadas em a, e ou o com s")]
+        [Theory(DisplayName = "Terminadas em á, â, é, ê, ó ou ô com s")]
         [InlineData("cajás")]
         [InlineData("jacarés")]
         [InlineData("avôs")]
@@ -32,7 +32,7 @@ namespace PluralizationServices.Tests.TonicAccent {
             Assert.True(_tonicAccent.Verify(word));
         }
 
-        [Theory(DisplayName = "Terminadas em a, e ou o seguidas de pronomes")]
+        [Theory(DisplayName = "Terminadas em á, â, é, ê, ó ou ô seguidas de pronomes")]
         [InlineData("amá-la")]
         [InlineData("dizê-lo")]
         [InlineData("pô-los")]
@@ -41,10 +41,37 @@ namespace PluralizationServices.Tests.TonicAccent {
             Assert.True(_tonicAccent.Verify(word));
         }
 
-        [Theory(DisplayName = "Terminadas com em ou ens")]
+        [Theory(DisplayName = "Terminadas com ém ou éns")]
         [InlineData("alguém")]
         [InlineData("nenéns")]
         public void TerminadoComEmOuEns(string word)
+        {
+            Assert.True(_tonicAccent.Verify(word));
+        }
+
+        [Theory(DisplayName = "Terminadas em i, í, u ou ú")]
+        [InlineData("aqui")]
+        [InlineData("saí")]
+        [InlineData("bambu")]
+        [InlineData("baú")]
+        public void TerminadoEmIU(string word) 
+        {
+            Assert.True(_tonicAccent.Verify(word));
+        }
+
+        [Theory(DisplayName = "Terminadas em i, í, u ou ú com s")]
+        [InlineData("gurupis")]
+        [InlineData("urubus")]
+        [InlineData("baús")]
+        public void TerminadoEmIUComS(string word) 
+        {
+            Assert.True(_tonicAccent.Verify(word));
+        }
+
+        [Theory(DisplayName = "Terminadas em i, í, u ou ú seguidas de pronomes")]
+        [InlineData("feri-lo")]
+        [InlineData("atribuí-lo")]
+        public void TerminadoEmIUSeguidaDePronome(string word) 
         {
             Assert.True(_tonicAccent.Verify(word));
         }
